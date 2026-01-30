9d6a642cece3d783e525c3a7b255a50d2a9b4371
Kyle 🐆 <kyle@privkey.io>
2026-01-30 16:57:01 -0500

    Bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.privkey.keep"
         minSdk = 33  // Android 13 for UniFFI Cleaner API
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
