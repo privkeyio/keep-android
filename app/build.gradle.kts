e709cfc9dd09a60356d7337fcf2b79fcfe63d275
Kyle 🐆 <kyle@privkey.io>
2026-05-29 19:43:53 -0400

    release: bump to v1.0.4 (pin keep v0.4.7) (#282)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.privkey.keep"
         minSdk = 33
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.0.3"
+        versionCode = 16
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
