05f45909c242f1520be2cc521de45c454c6af7ad
Kyle 🐆 <kyle@privkey.io>
2026-06-13 19:50:23 -0400

    release: keep-android v1.1.0 (pin keep v0.4.9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.privkey.keep"
         minSdk = 33
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.0.5"
+        versionCode = 18
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
