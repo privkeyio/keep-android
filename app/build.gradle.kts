5124087186f878d4b265072930675503ecc9582b
Kyle 🐆 <kyle@privkey.io>
2026-06-14 10:31:55 -0400

    release: keep-android v1.1.2 (fix reproducible release signing) (#348)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.privkey.keep"
         minSdk = 33
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.1.1"
+        versionCode = 20
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
