1f014968a30d261c03e04dcb2bd3d684fdd88894
Kyle 🐆 <kyle@privkey.io>
2026-06-14 08:56:18 -0400

    release: keep-android v1.1.1 (reproducible container build) (#345)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.privkey.keep"
         minSdk = 33
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.1.0"
+        versionCode = 19
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
